2e2205c884f30752263b947b39d1231d050a6886
Brandyn Sullins <woofenwauzer@gmail.com>
2019-02-02 12:50:47 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Chris AND Brandyn are both Gods
+            Chris AND Brandyn are both Gods!!
           </h1>
         </header>
       </div>
