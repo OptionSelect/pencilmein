0bcab962e3bf56c15b4cbbc491cebcd87908cc67
Chris <chrispackett1998@gmail.com>
2019-02-02 13:53:13 -0500

    added bootstrap and a dateheader component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import DateHeader from './Components/DateHeader';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dates: ['February 2nd, 2019', 'February 3rd, 2019', 'February 5th, 2019'],
+      events: []
+    }
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Chris AND Brandyn are both Gods!!
-          </h1>
-        </header>
+      <div>
+        <nav class="navbar sticky-top navbar-dark bg-dark mb-5">
+        <span class="navbar-brand">
+            <span class="ml-1">I'm In</span>
+        </span>
+        </nav>
+        <div>
+          <DateHeader dateHeaders={this.state.dates} />
+        </div>
       </div>
     );
   }
